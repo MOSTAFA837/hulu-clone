e9c25b229f7ba051cecff786ef4e9d27efe7157c
MOSTAFA837 <mostafamanchester837@gmail.com>
2022-01-30 13:05:36 +0200

    second commit

--- components/Thumbnail.js
+++ components/Thumbnail.js
@@ -2,17 +2,17 @@ import Image from "next/image"
 import { ThumbUpIcon } from "@heroicons/react/outline";
 
 const Thumbnail = ({result}) => {
-    console.log(result)
+    // console.log(result)
 
     const BASE_URL = "https://image.tmdb.org/t/p/original/";
-
+    
   return (
     <div className="group cursor-pointer p-2 transition 
         duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
       <Image layout="responsive" 
+        priority="true"
         src={`
-            ${BASE_URL}${result.backdrop_path || 
-            result.poster_path }` ||
+            ${BASE_URL}${result.backdrop_path}` ||
             `${BASE_URL}${result.poster_path}`
         }
         height={1080} width={1920}
@@ -21,7 +21,7 @@ const Thumbnail = ({result}) => {
       <div className="p-2">
           <p className="truncate max-w-md">{result.overview}</p>
           <h2 className="mt-1 text-2xl text-white 
-            duration-100 ease-in-out group-hover:font-bold ">{result.titletitle || result.original_title}</h2>
+            duration-100 ease-in-out group-hover:font-bold ">{result.title || result.original_title}</h2>
           <p className="flex items-center opacity-0
             group-hover:opacity-100">
              {result.media_type && `${result.media_type} .`} {" "}
